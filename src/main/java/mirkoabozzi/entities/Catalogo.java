e74cb7fb35994517a96b5b9515ffb37b1331b1bd
Mirko Abozzi <mirko.abozzi@gmail.com>
2024-08-02 09:26:24 +0200

    Initial commit

---
+++ src/main/java/mirkoabozzi/entities/Catalogo.java
@@ -0,0 +1,49 @@
+package mirkoabozzi.entities;
+
+import java.time.LocalDate;
+
+public class Catalogo {
+    private int isbn;
+    private String titolo;
+    private LocalDate annoPubblicazione;
+    private int numeroPagine;
+
+    public Catalogo(int isbn, String titolo, LocalDate annoPubblicazione, int numeroPagine) {
+        this.isbn = isbn;
+        this.titolo = titolo;
+        this.annoPubblicazione = annoPubblicazione;
+        this.numeroPagine = numeroPagine;
+    }
+
+    public int getIsbn() {
+        return isbn;
+    }
+
+    public void setIsbn(int isbn) {
+        this.isbn = isbn;
+    }
+
+    public String getTitolo() {
+        return titolo;
+    }
+
+    public void setTitolo(String titolo) {
+        this.titolo = titolo;
+    }
+
+    public LocalDate getAnnoPubblicazione() {
+        return annoPubblicazione;
+    }
+
+    public void setAnnoPubblicazione(LocalDate annoPubblicazione) {
+        this.annoPubblicazione = annoPubblicazione;
+    }
+
+    public int getNumeroPagine() {
+        return numeroPagine;
+    }
+
+    public void setNumeroPagine(int numeroPagine) {
+        this.numeroPagine = numeroPagine;
+    }
+}
